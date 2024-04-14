ae181ccfadf229c1ef486a1223595b567ac81a84
navyaraina <navyaraina77@gmail.com>
2024-04-14 13:47:33 +0530

    BSP-Trees

---
+++ BSPGenerator.cs
@@ -0,0 +1,119 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BSPGenerator : MonoBehaviour
+{
+    public int depth = 5;
+    public float minWidth = 5f;
+    public float minHeight = 5f;
+    public float maxVariation = 3f;
+
+    public GameObject groundPrefab;
+    public GameObject wallPrefab;
+
+    //for partitioning the entire cave prefab into different rooms
+
+    public class BSPNode
+    {
+        public Rect partition;
+        public BSPNode leftChild;
+        public BSPNode rightChild;
+
+        public BSPNode(Rect partition)
+        {
+            this.partition = partition;
+        }
+    }
+
+    //instantiates the first partitioning and prefabs for passages
+
+    void Start()
+    {
+        BSPNode root = new BSPNode(new Rect(0, 0, 100, 100));
+
+        SplitPartition(root, depth);
+    }
+
+    void SplitPartition(BSPNode node, int remainingDepth)
+    {
+
+        //at lowest partition, generate wall if space, else floor
+        if (remainingDepth <= 0)
+        {
+            GenerateTerrain(node.partition);
+            return;
+        }
+
+        //randomized terrain splitting/generation
+        bool splitHorizontal = Random.value > 0.5f;
+        float splitPosition;
+        if (splitHorizontal)
+        {
+            splitPosition = Random.Range(node.partition.yMin + minHeight, node.partition.yMax - minHeight);
+
+            node.leftChild = new BSPNode(new Rect(node.partition.xMin, node.partition.yMin, node.partition.width, splitPosition - node.partition.yMin));
+            node.rightChild = new BSPNode(new Rect(node.partition.xMin, splitPosition, node.partition.width, node.partition.yMax - splitPosition));
+        }
+        else
+        {
+            splitPosition = Random.Range(node.partition.xMin + minWidth, node.partition.xMax - minWidth);
+
+            node.leftChild = new BSPNode(new Rect(node.partition.xMin, node.partition.yMin, splitPosition - node.partition.xMin, node.partition.height));
+            node.rightChild = new BSPNode(new Rect(splitPosition, node.partition.yMin, node.partition.xMax - splitPosition, node.partition.height));
+        }
+
+        //if (splitHorizontal)
+        //{
+        //    //node.leftChild = new BSPNode(new Rect(node.partition.xMin, node.partition.yMin, node.partition.width, splitPosition - node.partition.yMin));
+        //    //node.rightChild = new BSPNode(new Rect(node.partition.xMin, splitPosition, node.partition.width, node.partition.yMax - splitPosition));
+        //}
+        //else
+        //{
+        //    //node.leftChild = new BSPNode(new Rect(node.partition.xMin, node.partition.yMin, splitPosition - node.partition.xMin, node.partition.height));
+        //    //node.rightChild = new BSPNode(new Rect(splitPosition, node.partition.yMin, node.partition.xMax - splitPosition, node.partition.height));
+        //}
+
+        SplitPartition(node.leftChild, remainingDepth - 1);
+        SplitPartition(node.rightChild, remainingDepth - 1);
+    }
+
+    void GenerateTerrain(Rect partition)
+    {
+        //if (partition.width < minWidth || partition.height < minHeight)
+        //{
+            Vector3 groundPosition = new Vector3(partition.center.x, partition.center.y, partition.center.x);
+            Instantiate(groundPrefab, groundPosition, Quaternion.identity);
+            float wallThickness = 1f;
+
+            Vector3 leftWallPosition = new Vector3(partition.xMin, partition.center.y, partition.center.x);
+            Vector3 leftWallScale = new Vector3(wallThickness, 1, partition.height);
+            Instantiate(wallPrefab, leftWallPosition, Quaternion.identity).transform.localScale = leftWallScale;
+        //}
+        //else
+        //{
+        //    GenerateWalls(partition);
+        //}
+    }
+
+    //void GenerateWalls(Rect partition)
+    //{
+    //    float wallThickness = 1f;
+
+    //    Vector3 leftWallPosition = new Vector3(partition.xMin, partition.center.y, partition.center.x);
+    //    Vector3 leftWallScale = new Vector3(wallThickness, 1, partition.height);
+    //    Instantiate(wallPrefab, leftWallPosition, Quaternion.identity).transform.localScale = leftWallScale;
+
+    //    //Vector3 rightWallPosition = new Vector3(partition.xMax, 0, partition.center.y);
+    //    //Vector3 rightWallScale = new Vector3(wallThickness, 1, partition.height);
+    //    //Instantiate(wallPrefab, rightWallPosition, Quaternion.identity).transform.localScale = rightWallScale;
+
+    //    //Vector3 frontWallPosition = new Vector3(partition.center.x, 0, partition.yMin);
+    //    //Vector3 frontWallScale = new Vector3(partition.width, 1, wallThickness);
+    //    //Instantiate(wallPrefab, frontWallPosition, Quaternion.identity).transform.localScale = frontWallScale;
+
+    //    //Vector3 backWallPosition = new Vector3(partition.center.x, 0, partition.yMax);
+    //    //Vector3 backWallScale = new Vector3(partition.width, 1, wallThickness);
+    //    //Instantiate(wallPrefab, backWallPosition, Quaternion.identity).transform.localScale = backWallScale;
+    //}
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # BSP-Tree-Integration
 Integrating BSP trees for environment regeneration in Unity-3D using C#
+
+
+## Integration
+Attach this code to an empty game object, and assign prefabs for ground and wall regeneration. If you wish to have a variety of prefabs, convert this code block into a list!
+    public GameObject groundPrefab;
+    public GameObject wallPrefab;
+
+Ensure that your prefabs and the axis in the code match.
